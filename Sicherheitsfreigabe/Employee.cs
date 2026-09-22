a56008a092a676fdd77ab1efe8586fa22068e59d
ciennce <maxileonhoppe@googlemail.com>
2026-09-22 12:47:38 +0200

    added more dependencies to employee

--- Sicherheitsfreigabe/Employee.cs
+++ Sicherheitsfreigabe/Employee.cs
@@ -4,15 +4,19 @@
     {
         private int employeeId { get; set; }
         private string name { get; set; }
+        private DateTime hiredDate { get; set; }
+        private DateTime birthday { get; set; };
         private bool isOnVacation { get; set; }
         private bool isOnBusinessTrip { get; set; }
 
-        public Employee(string pName, int pEmployeeId, bool pIsOnBusinessTrip, bool pIsOnVacation)
+        public Employee(string pName, int pEmployeeId, bool pIsOnBusinessTrip, bool pIsOnVacation, DateTime pHiredDate, DateTime pBirthday)
         {
             name = pName;
             employeeId = pEmployeeId;
             isOnBusinessTrip = pIsOnBusinessTrip;
             isOnVacation = pIsOnVacation;
+            hiredDate = pHiredDate;
+            birthday = pBirthday;
         }
 
         public static bool isAvailable(Employee employee)
